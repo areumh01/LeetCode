56d2246ecedfd4ce82ef924e5da8155fbd50f184
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-07-20 15:24:31 +0900

    Time: 15 ms (71.71%), Space: 17.8 MB (55.66%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,37 +2,28 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> answer;
-        deque<int> dq;
+        stack<int> s;
         for(auto e:asteroids){
-            if(e>0) dq.push_back(e);
+            if(e>0) s.push(e);
             else{
-                if(dq.empty()) dq.push_back(e);
+                if(s.empty() || s.top() < 0) s.push(e);
                 else{
-                    if(dq.back()<0) dq.push_back(e);
-                    else{
-                        bool flag = true;
-                        while(!dq.empty() && dq.back() >0){
-                            if(dq.back() < -e){
-                                dq.pop_back();
-                            }
-                            else if(dq.back() == -e){
-                                dq.pop_back();
-                                flag = false;
-                                break;
-                            }
-                            else{
-                                flag = false;
-                                break;
-                            }
-                        }
-                        if(flag) dq.push_back(e);
+                    while(!s.empty() && s.top()>0 && s.top()<-e){
+                        s.pop();
                     }
+                    //s가 비거나 s.top이 음수가 되거나 e의 절댓값보다 커서 탈출
+                    if(!s.empty() && s.top()==-e) s.pop();
+                    else if(s.empty() || s.top()<0) s.push(e);
                 }
             }
         }
-        while(!dq.empty()){
-            answer.push_back(dq.front());
-            dq.pop_front();
+        answer.resize(s.size());
+        int size = s.size();
+        int i=1;
+        while(!s.empty()){
+            answer[size-i] = s.top();
+            s.pop();
+            i++;
         }
         return answer;
     }
