4f17a54a5c4f25a0153e5505ce8a94cb9816ef76
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-07-21 12:39:08 +0900

    Time: 24 ms (20.55%), Space: 5.8 MB (81.16%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,16 +3,9 @@ public:
     bool isPalindrome(int x) {
         if(x<0) return false;
         string s = to_string(x);
-        int start=0;
-        int end=s.size()-1;
-        
-        while(start<=end){
-            if(s[start]==s[end]){
-                start++;
-                end--;
-            }
-            else return false;
-        }
-        return true;
+        string tmp=s;
+        reverse(s.begin(),s.end());
+        if(s==tmp) return true;
+        else return false;
     }
 };
