2cd365f12e384c0b1321fe1a0a33f7cbc1547c39
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-07-21 12:49:30 +0900

    Time: 23 ms (24.96%), Space: 5.8 MB (81.16%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,32 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int length = 1;
-        stack<int> s;
-        int tmp=x;
-        
         if(x<0 || x!=0&&x%10==0) return false;
-        
-        while(true){
-            if(tmp/10 == 0) break;
-            tmp/=10;
-            length++;
-        }
-        //cout << length;
-        
-        for(int i=0; i<length/2; i++){
-            s.push(x%10);
+        int check=0;
+        while(x>check){
+            check=check*10+x%10;
             x/=10;
         }
-        if(length%2==1) x/=10;
-        
-        while(x!=0){
-            if(x%10 == s.top()){
-                s.pop();
-                x/=10;
-            }
-            else return false;
-        }
-        return true;
+        if(x==check||check/10==x) return true;
+        else return false;
     }
 };
