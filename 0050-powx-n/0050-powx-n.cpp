18d5a1fe248785820cae782f55cfec466bc0eb30
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-07-24 12:38:03 +0900

    Time: 4 ms (40.18%), Space: 5.9 MB (92.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,34 +1,14 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        //if(n<0) x=1/x;
-        //
-        //int num = labs(n);
-        //
-        //double pow=1;
-        //while(num){
-        //    if(num&1) pow*=x;
-        //    x*=x;
-        //    num >>= 1;
-        //}
-        //
-        //return pow;
-        
-        
-        if(n < 0) {
-            x = 1 / x;
-        } 
+        if(n<0) x=1/x;
         
         long num = labs(n);
         
-        double pow = 1;
-        
-        while(num){ // equivalent to while(num != 0)
-            if(num & 1) { // equivalent to if((num & 1) != 0)
-                pow *= x;
-            }
-            
-            x *= x;
+        double pow=1;
+        while(num){
+            if(num&1) pow*=x;
+            x*=x;
             num >>= 1;
         }
         
