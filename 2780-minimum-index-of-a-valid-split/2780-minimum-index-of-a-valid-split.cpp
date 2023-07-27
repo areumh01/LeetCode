21d76932e808acbae1b03ccbf925b595a2fb92a4
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-07-27 13:56:23 +0900

    Time: 310 ms (14.96%), Space: 98.8 MB (16.09%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        map<int,int> m;
+        map<int,int> m1,m2;
         vector<int> freqArr;
         int dominant = 0;
         
         for(auto e: nums){
-            m[e]++;
-            if(m[dominant] < m[e]) dominant = e;
+            m2[e]++;
         }
         
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] == dominant) freqArr.push_back(i);
+            int x = --m2[nums[i]];
+            int y = ++m1[nums[i]];
+            if(y*2 > i+1 && x*2 > (nums.size()-i-1) ) return i;
         }
         
-        for(int i=1; i<=freqArr.size(); i++){
-            if(freqArr[i-1]+1<i*2)
-                if(nums.size()-(freqArr[i-1]+1) < (freqArr.size()-i)*2)
-                    return freqArr[i-1];
-        }
         return -1;
     }
 };
