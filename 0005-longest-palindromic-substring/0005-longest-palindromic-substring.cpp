db7d18d4af40a88794ccef82ee8ea4660ea79469
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-07-31 08:11:02 +0900

    Time: 151 ms (50.81%), Space: 35.1 MB (29.19%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -9,26 +9,12 @@ public:
         for(int len=0; len<s.size(); len++){
             for(int i=0; i+len<s.size(); i++){
                 int j=i+len;
-                if(i==j) dp[i][j]=true;
-                else if(s[i]==s[j]){
-                    if(len==1) dp[i][j]=true;
-                    else{
-                        if(dp[i+1][j-1]) dp[i][j]=true;
-                        else dp[i][j]=false;
-                    }
-                }
-                else dp[i][j]=false;
-            }
-        }
-        
-        for(int i=0; i<dp.size(); i++){
-            for(int j=i; j<dp.size(); j++){
-                if(dp[i][j]){
+                if(s[i]==s[j] && (j<=i+1 || dp[i+1][j-1])){
+                    dp[i][j]=true;
                     if(j-i+1 > answer.size()) answer = s.substr(i,j-i+1);
                 }
             }
         }
-        
         return answer;
     }
 };
