2204c816a4679ce1723c5f055e76a5ebdff56c8a
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-07-30 18:29:51 +0900

    Time: 20 ms (93.88%), Space: 9.5 MB (43.21%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,48 +1,49 @@
-//class Solution {
-//public:
-//    vector<vector<int>> dp{102,vector<int> {102,0}};
-//    
-//    int find_answer(int i, int j, string& s){
-//        if(dp[i][j] != 0) return dp[i][j];
-//        if(i==j) return dp[i][j]=1;
-//        if(s[i]==s[j] || s[j]==s[j-1]) return find_answer(i,j-1,s);
-//        if(s[i]==s[i+1]) return find_answer(i+1,j,s);
-//        
-//        int tmp = find_answer(i,j-1,s)+1;
-//        for(int k=i+1; k<j; k++){
-//            if(s[k]==s[j])
-//                tmp = min(tmp, find_answer(i,k-1,s)+find_answer(k,j,s));
-//        }
-//        return dp[i][j]=tmp;
-//    }
-//    
-//    int strangePrinter(string s) {
-//        return find_answer(0,s.size()-1, s);
-//    }
-//};
-
 class Solution {
 public:
-    int n;
     vector<vector<int>> dp;
-    int f(int i, int j, string& s){       
-        if (dp[i][j]!=-1) return dp[i][j];
-        if (i==j) return dp[i][j]=1;
-        int ans;
-        if (s[i]==s[j]||s[j-1]==s[j]) ans=f(i, j-1, s);
-        else if (s[i]==s[i+1]) ans=f(i+1, j, s);
-        else{
-            ans=f(i,j-1, s)+1;
-            for(int k=i+1; k<j; k++){
-                if(s[k]==s[j])
-                    ans=min(ans, f(i, k-1, s)+f(k, j-1, s));
-            }
+    
+    int find_answer(int i, int j, string& s){
+        if(dp[i][j] != 0) return dp[i][j];
+        if(i==j) return dp[i][j]=1;
+        if(s[i]==s[j] || s[j]==s[j-1]) return find_answer(i,j-1,s);
+        if(s[i]==s[i+1]) return find_answer(i+1,j,s);
+        
+        int tmp = find_answer(i,j-1,s)+1;
+        for(int k=i+1; k<j; k++){
+            if(s[k]==s[j])
+                tmp = min(tmp, find_answer(i,k-1,s)+find_answer(k,j,s));
         }
-        return dp[i][j]=ans;
+        return dp[i][j]=tmp;
     }
+    
     int strangePrinter(string s) {
-        n=s.size();
-        dp.assign(n, vector<int>(n, -1));
-        return f(0, n-1, s);
+        dp.assign(s.size(), vector<int>(s.size(), 0));
+        return find_answer(0,s.size()-1, s);
     }
 };
+
+//class Solution {
+//public:
+//    int n;
+//    vector<vector<int>> dp;
+//    int f(int i, int j, string& s){       
+//        if (dp[i][j]!=-1) return dp[i][j];
+//        if (i==j) return dp[i][j]=1;
+//        int ans;
+//        if (s[i]==s[j]||s[j-1]==s[j]) ans=f(i, j-1, s);
+//        else if (s[i]==s[i+1]) ans=f(i+1, j, s);
+//        else{
+//            ans=f(i,j-1, s)+1;
+//            for(int k=i+1; k<j; k++){
+//                if(s[k]==s[j])
+//                    ans=min(ans, f(i, k-1, s)+f(k, j-1, s));
+//            }
+//        }
+//        return dp[i][j]=ans;
+//    }
+//    int strangePrinter(string s) {
+//        n=s.size();
+//        dp.assign(n, vector<int>(n, -1));
+//        return f(0, n-1, s);
+//    }
+//};
