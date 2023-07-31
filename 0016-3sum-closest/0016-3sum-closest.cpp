be23f84969232b1e9f39c74062e7826536c751ba
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-07-31 09:18:49 +0900

    Time: 39 ms (95.00%), Space: 10.2 MB (15.60%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -5,15 +5,13 @@ public:
         
         sort(nums.begin(), nums.end());
         for(int i=0; i<nums.size(); i++){
-            int newTarget = target-nums[i];
-            
             int left=i+1;
             int right=nums.size()-1;
             
             while(left<right){
-                int sum = nums[left]+nums[right];
-                int subdiff = newTarget-sum;
-                diff = abs(diff) < abs(target-sum-nums[i])? diff:target-sum-nums[i];
+                int sum = nums[i]+nums[left]+nums[right];
+                int subdiff = target-sum;
+                diff = abs(diff) < abs(subdiff)? diff:subdiff;
                 if(subdiff<0) right--;
                 else if(subdiff>0) left++;
                 else break;
