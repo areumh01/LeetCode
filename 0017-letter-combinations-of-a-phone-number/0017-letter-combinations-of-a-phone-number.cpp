e508443679fb3a77b422b5c54df97e61fd5a1d3a
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-08-01 11:39:15 +0900

    Time: 3 ms (37.73%), Space: 6.6 MB (31.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,47 +1,35 @@
 class Solution {
 public:
-    vector<string> answer;
-    
-    void dfs(int index, int counting, string& subs, string& s){
-        int num = (s[index]-'0');
-
-        if(num>=2 && num<=6){
-            num = 3*(num-2);
-            for(int i=0; i<3; i++){
-                string tmp = subs;
-                tmp+=('a'+num+i);
-                if(counting == s.size()-1) answer.push_back(tmp);
-                else dfs(index+1, counting+1, tmp, s);
-            }
-        }
-        else{
-            if(num==7 || num==9){
-                if(num==7) num=15;
-                if(num==9) num=22;
-                for(int i=0; i<4; i++){
-                    string tmp = subs;
-                    tmp+=('a'+num+i);
-                    if(counting == s.size()-1) answer.push_back(tmp);
-                    else dfs(index+1, counting+1, tmp, s);
-                }
-            }
-            else{
-                num=19;
-                for(int i=0; i<3; i++){
-                    string tmp = subs;
-                    tmp+=('a'+num+i);
-                    if(counting == s.size()-1) answer.push_back(tmp);
-                    else dfs(index+1, counting+1, tmp, s);
-                }
-            }
-        }
-    }
-    
     vector<string> letterCombinations(string digits) {
         if(digits.size()==0) return {};
+        vector<string> nums = {"", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> answer;
+        vector<char> tmp(digits.size());
+        int i=0; //for digits
+        int k=0; //for "abc", "def" etc...
+        stack<int> s;
         
-        string s = "";
-        dfs(0,0,s,digits);
+        while(i>=0){
+            int index = digits[i]-'0';
+            string dials = nums[index-1]; // "abc", "def" etc..
+            tmp[i] = dials[k++];
+            if(k>dials.size()){
+                i--;
+                if(s.empty()) break;
+                k=s.top();
+                s.pop();
+            }
+            else if(i==digits.size()-1){
+                string sub="";
+                for(auto e:tmp) sub+=e;
+                answer.push_back(sub);
+            }
+            else{
+                i++;
+                s.push(k);
+                k=0;
+            }
+        }
         
         return answer;
     }
