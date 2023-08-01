dac51e621755b346956ede3f6660df7b02871be1
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-08-01 10:13:54 +0900

    Time: 18 ms (87.68%), Space: 8.9 MB (83.81%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> answer;
-    int N,K;
-    
-    void make_comb(int index, int counting, vector<int>& nums){
-        for(int i=index+1; i<=N; i++){
-            vector<int> tmp = nums;
-            tmp.push_back(i);
-            if(counting == K-1) answer.push_back(tmp);
-            else make_comb(i, counting+1, tmp);
-        }
-    }
-    
     vector<vector<int>> combine(int n, int k) {
-        N=n;
-        K=k;
-        vector<int> first = {};
-        make_comb(0,0,first);
+        vector<vector<int>> answer;
+        vector<int> p(k,0);
+        int i=0;
+        
+        while(i>=0){
+            p[i]++;
+            if(p[i]>n) i--;
+            else if(i==k-1) answer.push_back(p);
+            else{
+                i++;
+                p[i] = p[i-1];
+            }
+        }
+        
         return answer;
     }
 };
