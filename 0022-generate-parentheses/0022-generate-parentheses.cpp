79df81f8e9aac274faddb1ccd099fe4517176272
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-08-03 12:32:35 +0900

    Time: 3 ms (76.99%), Space: 13.7 MB (57.79%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    vector<vector<string>> dp{10};
+    vector<string> answer;
+    
+    void makeparen(string str, int n, int m){
+        if(n==0&&m==0) answer.push_back(str);
+        
+        if(n>0) makeparen(str+"(",n-1,m+1);
+        if(m>0) makeparen(str+")",n,m-1);
+    }
     
     vector<string> generateParenthesis(int n) {
-        dp[1] = {"()"};
-        for(int i=2; i<=n; i++){
-            vector<string> c;
-            for(int j=1; j<i; j++){
-                vector<string> a = dp[j];
-                vector<string> b = dp[i-j];
-                if(j==1)
-                   for(auto el : b) c.push_back("("+el+")");
-                for(auto e : a){
-                    for(auto el : b){
-                        c.push_back(e+el);
-                        c.push_back(el+e);
-                    }
-                }
-            }
-            sort(c.begin(),c.end());
-            c.erase(unique(c.begin(),c.end()),c.end());
-            dp[i]=c;
-        }
-        return dp[n];
+        makeparen("", n, 0);
+        return answer;
     }
 };
