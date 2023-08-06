ce25dfff3f7aa29e2c27eeb83273fca64c30ed7f
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-08-06 13:28:41 +0900

    Time: 8 ms (28.48%), Space: 12.2 MB (20.04%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
+    vector<int> tmp;
+    
+    void next_perm(int index){
+        cout << index << '\n';
+        int number;
+        for(int i=tmp.size()-1; i>index; i--){
+            if(tmp[index] < tmp[i]){
+                number=tmp[i];
+                tmp[i] = tmp[index];
+                tmp[index] = number;
+                break;
+            }
+        }
+        sort(tmp.begin()+index+1, tmp.end());
+    }
+    
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        tmp = nums;
+        bool flag = false;
+        for(int i=nums.size()-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                next_perm(i);
+                flag = true;
+                break;
+            }
+        }
+        if(!flag) sort(tmp.begin(),tmp.end());
+        nums = tmp;
     }
 };
