270345743639aab1c3728af836efa739cbbea416
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-08-08 14:32:04 +0900

    Time: 0 ms (100.00%), Space: 11.1 MB (13.70%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int bs(int start, int end, vector<int>& nums, int target){
-        int mid = (start+end)/2;
-        while(start<=end){
-            mid = (start+end)/2;
-            if(nums[mid]<target) start = mid+1;
-            else if(nums[mid]>target) end = mid-1;
-            else break;
-        }
-        if(nums[mid]==target) return mid;
-        else return -1;
-    }
-    
     int search(vector<int>& nums, int target) {
-        if(nums[0]<nums.back()) return bs(0,nums.size()-1,nums,target);
-        int start =0;
-        int end = nums.size()-1;
-        while(start<end-1){
+        int start=0;
+        int end=nums.size()-1;
+        while(start<end){
             int mid = (start+end)/2;
-            if(nums[start] < nums[mid]) start = mid;
-            else if(nums[end] > nums[mid]) end = mid;
+            if(nums[mid]>nums.back()) start=mid+1;
+            else end=mid;
         }
-        if(target >= nums[0]) return bs(0, start, nums, target);
-        else return bs(start+1, nums.size()-1, nums, target);
+        int last = end;
+        start=0;
+        end=nums.size()-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            int realmid = (mid+last)%nums.size();
+            if(nums[realmid]<target) start=mid+1;
+            else if(nums[realmid]>target) end=mid-1;
+            else return realmid;
+        }
+        return -1;
     }
 };
