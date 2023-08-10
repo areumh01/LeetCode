ffa04f01c25ecce5fb1a53db825eb2ed05910ede
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-08-10 12:36:29 +0900

    Time: 3 ms (74.13%), Space: 11.1 MB (15.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,20 +3,24 @@ public:
     int search(vector<int>& nums, int target) {
         int start=0;
         int end=nums.size()-1;
+        
         while(start<end){
-            int mid = (start+end)/2;
-            if(nums[mid]>nums.back()) start=mid+1;
+            int mid=(start+end)/2;
+            if(nums[mid]>=nums.back()) start=mid+1;
             else end=mid;
         }
-        int last = end;
+        
+        int inverse = end;
+        //cout << inverse << '\n';
         start=0;
         end=nums.size()-1;
+        
         while(start<=end){
             int mid=(start+end)/2;
-            int realmid = (mid+last)%nums.size();
-            if(nums[realmid]<target) start=mid+1;
-            else if(nums[realmid]>target) end=mid-1;
-            else return realmid;
+            int pivottedmid=(mid+inverse)%nums.size();
+            if(nums[pivottedmid] > target) end = mid-1;
+            else if(nums[pivottedmid] < target) start=mid+1;
+            else return pivottedmid;
         }
         return -1;
     }
