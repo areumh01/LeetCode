e8437c5fe48fa0aca03e46b97c7f0ba617433fb8
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-08-24 14:30:09 +0900

    Time: 56 ms (35.38%), Space: 91.7 MB (32.02%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -3,11 +3,13 @@ public:
     bool repeatedSubstringPattern(string s) {
         int n = s.size();
         for(int i=0; i<n/2; i++){
-            string sub = s.substr(0,i+1);
-            string comp = s.substr(i+1);
-            while(comp.find(sub) == 0){
-                if(comp.find(sub)+sub.size() == comp.size()) return true;
-                comp = comp.substr(comp.find(sub)+sub.size());
+            if(n%(i+1)==0){
+                string sub = s.substr(0,i+1);
+                string repeated = "";
+                for(int t=0; t<n/(i+1); t++){
+                    repeated+=sub;
+                }
+                if(repeated == s) return true;
             }
         }
         return false;
