91c8cdc012748f5002aa6c3b6056894673ff0ab4
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-09-10 15:18:12 +0900

    Time: 10 ms (23.64%), Space: 12.4 MB (19.02%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     vector<vector<int>> answer;
 
-    void find_answer(vector<int>& candidates, int target, int index, vector<int> result){
-        if(target == 0) {
-            answer.push_back(result);
+    void dfs(vector<int>& candidates, int target, int counting, int index, vector<int> sub){
+        if(target == counting){
+            answer.push_back(sub);
             return;
         }
         for(int i=index; i<candidates.size(); i++){
             if(i>index && candidates[i]==candidates[i-1]) continue;
-            vector<int> tmp = result;
+            if(counting+candidates[i] > target) continue;
+            vector<int> tmp = sub;
             tmp.push_back(candidates[i]);
-            if(target >= candidates[i])
-                find_answer(candidates, target-candidates[i], i+1, tmp);
+            dfs(candidates,target,counting+candidates[i], i+1, tmp);
         }
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        //for(auto e : candidates) cout << e << " ";
-        vector<int> visited;
-        find_answer(candidates, target, 0, visited);
-        sort(answer.begin(),answer.end());
-        answer.erase(unique(answer.begin(), answer.end()), answer.end());
+        dfs(candidates,target,0,0,{});
         return answer;
     }
 };
