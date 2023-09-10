4a1b4123d6327807afe2cfc532f8e1c9e12075e2
HAN AH REUM <128774701+areumh01@users.noreply.github.com>
2023-09-10 20:28:01 +0900

    Time: 5 ms (11.78%), Space: 7.1 MB (16.34%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> answer;
+    bool visited[10];
+
+    void dfs(int k, int n, int counting, int index, vector<int> sub){
+        if(sub.size() == k && counting == n){
+            answer.push_back(sub);
+            return;
+        }
+        for(int i=index; i<10; i++){
+            if(!visited[i] && sub.size() < k && counting+i <= n){
+                visited[i] = true;
+                vector<int> tmp = sub;
+                tmp.push_back(i);
+                dfs(k,n,counting+i,i+1,tmp);
+                visited[i] = false;
+            }
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        memset(visited,false,sizeof(visited));
+        dfs(k,n,0,1,{});
+        return answer;
+    }
+};
